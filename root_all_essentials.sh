1bb61af27296faaf151eb0c14277074b21458bce
Leonardo <santos.leonardocaloi@gmail.com>
2023-04-10 00:06:56 -0300

    root fixes

--- root_all_essentials.sh
+++ root_all_essentials.sh
@@ -2,14 +2,11 @@
 
 source .env
 
-# Armazene o diretório atual em uma variável
-CURRENT_DIR=$(pwd)
-
 chmod -R +x ./essentials/*
 
 ./essentials/add_sudo_user.sh $USERNAME $PASSWORD
 
 # Use a variável do diretório atual para executar os scripts
-su - $USERNAME -c "${CURRENT_DIR}/essentials/basic.sh"
-su - $USERNAME -c "${CURRENT_DIR}/essentials/devops.sh"
-su - $USERNAME -c "${CURRENT_DIR}/essentials/oh-my-zsh.sh"
+su - $USERNAME -c "$(pwd)/essentials/basic.sh"
+su - $USERNAME -c "$(pwd)/essentials/devops.sh"
+su - $USERNAME -c "$(pwd)/essentials/oh-my-zsh.sh"
